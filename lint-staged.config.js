9e37b05b77cc74c46ac329951795fb2e2dccdb2a
Lenin garizabalo <lenin.garizabalo@sundevs.com>
2022-07-01 15:25:55 -0500

    add next lint script

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -1,9 +1,14 @@
+const path = require('path');
+
 module.exports = {
   // Type check TypeScript files
   '**/*.(ts|tsx)': () => 'yarn tsc --noEmit',
 
   // Lint then format TypeScript and JavaScript files
   '**/*.(ts|tsx|js)': (filenames) => [
+    `yarn next lint --fix --file ${filenames
+      .map((f) => path.relative(process.cwd(), f))
+      .join(' --file ')}`,
     `yarn eslint --fix ${filenames.join(' ')}`,
     `yarn prettier --write ${filenames.join(' ')}`,
   ],
@@ -11,4 +16,4 @@ module.exports = {
   // Format MarkDown and JSON
   '**/*.(md|json)': (filenames) =>
     `yarn prettier --write ${filenames.join(' ')}`,
-}
+};
